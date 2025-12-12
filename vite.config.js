c4b63039b78590b6c4e8c2989decf3b969da303f
5iravich <siravich.su@gmail.com>
2025-12-12 09:12:18 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/5ave-5core/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()]
+  // no base
 })
